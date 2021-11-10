d640164ccc9d2d9e89cb96609049e3cbd78ed85d
test <fachmi.bas@gmail.com>
2021-11-10 14:17:13 +0700

    innitial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       	<p>
+	  Hello There, node_modules Folder Has Been Deleted.
+	<br />Problem with node_modules permission, solved by change folder owner.
+	</p>
+	<p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
