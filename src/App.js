5f65a8b3825c0e18ba7f2113e7ff5edf35f706f9
test <fachmi.bas@gmail.com>
2022-01-13 14:00:04 +0700

    Adding Github Action config file & deploy to AWS ELB.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        	<p>
-	  Hello There, node_modules Folder Has Been Deleted.
+	  Hello There, container has been rebuild.
 	<br />Problem with node_modules permission, solved by change folder owner.
 	</p>
 	<p>
